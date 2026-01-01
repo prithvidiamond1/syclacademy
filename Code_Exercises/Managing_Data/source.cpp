1159d1083d85e5616f1d6c576cc3c694a3210e4e
Prithvi <prithviraj645@gmail.com>
2026-01-01 07:21:39 -0500

    finished ex03: Managing Data

--- Code_Exercises/Managing_Data/source.cpp
+++ Code_Exercises/Managing_Data/source.cpp
@@ -47,13 +47,33 @@
 
 #include "../helpers.hpp"
 
+#include <sycl/sycl.hpp>
+
 void test_usm() {
   int a = 18, b = 24, r = 0;
 
   // Task: Compute a+b on the SYCL device using USM
-  r = a + b;
+  // r = a + b;
+  auto q = sycl::queue();
+
+  auto a_dptr = sycl::malloc_device<int>(sizeof(a), q);
+  auto b_dptr = sycl::malloc_device<int>(sizeof(b), q);
+  auto r_dptr = sycl::malloc_device<int>(sizeof(r), q);
+
+  q.memcpy(a_dptr, &a, sizeof(a));
+  q.memcpy(b_dptr, &b, sizeof(b));
+
+  q.single_task([=]() {
+    r_dptr[0] = a_dptr[0] + b_dptr[0];
+  }).wait();  // wait is required since unlike streams in CUDA, queues in SYCL are not in-order.  
+  
+  q.memcpy(&r, r_dptr, sizeof(r)).wait();
 
   SYCLACADEMY_ASSERT_EQUAL(r, 42);
+
+  sycl::free(a_dptr, q);
+  sycl::free(b_dptr, q);
+  sycl::free(r_dptr, q);
 }
 
 void test_buffer() {
@@ -61,7 +81,22 @@ void test_buffer() {
 
   // Task: Compute a+b on the SYCL device using the buffer
   // accessor memory model
-  r = a + b;
+  // r = a + b;
+
+  auto a_buf = sycl::buffer(&a, sycl::range<1>(1));
+  auto b_buf = sycl::buffer(&b, sycl::range<1>(1));
+  auto r_buf = sycl::buffer(&r, sycl::range<1>(1));
+
+  auto q = sycl::queue();
+  q.submit([&](sycl::handler &cgh) {
+    auto a_acc = sycl::accessor(a_buf, cgh, sycl::read_only);
+    auto b_acc = sycl::accessor(b_buf, cgh, sycl::read_only);
+    auto r_acc = sycl::accessor(r_buf, cgh, sycl::write_only);
+
+    cgh.single_task([=](){
+      r_acc[0] = a_acc[0] + b_acc[0];
+    });
+  }).wait();
 
   SYCLACADEMY_ASSERT_EQUAL(r, 42);
 }
