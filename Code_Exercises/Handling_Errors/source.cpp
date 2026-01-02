0c524efb44c7d6246df1ef16fa00e8e7dbe289f4
Prithvi <prithviraj645@gmail.com>
2026-01-02 12:17:48 -0500

    Completed ex04: Handling Error

--- Code_Exercises/Handling_Errors/source.cpp
+++ Code_Exercises/Handling_Errors/source.cpp
@@ -8,6 +8,10 @@
  work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 */
 
+#include <exception>
+#include <sycl/aspects.hpp>
+#include <sycl/device_selector.hpp>
+#include <sycl/exception_list.hpp>
 #include <sycl/sycl.hpp>
 
 #include "../helpers.hpp"
@@ -15,19 +19,28 @@
 int main() {
   // Task: catch synchronous and asynchronous exceptions
 
-  auto defaultQueue = sycl::queue{};
+  try{
+    auto defaultQueue = sycl::queue{
+       [=](sycl::exception_list el) {
+        for (auto e: el) {
+          std::rethrow_exception(e);
+        }
+    }};
 
-  auto buf = sycl::buffer<int>(sycl::range{1});
+    auto buf = sycl::buffer<int>(sycl::range{1});
 
-  defaultQueue
-      .submit([&](sycl::handler& cgh) {
-        // This throws an exception: an accessor has a range which is
-        // outside the bounds of its buffer.
-        auto acc = buf.get_access(cgh, sycl::range{2}, sycl::read_write);
-      })
-      .wait();
+    defaultQueue
+        .submit([&](sycl::handler& cgh) {
+          // This throws an exception: an accessor has a range which is
+          // outside the bounds of its buffer.
+          auto acc = buf.get_access(cgh, sycl::range{2}, sycl::read_write);
+        })
+        .wait();
 
-  defaultQueue.throw_asynchronous();
-
+    defaultQueue.throw_asynchronous();
+  } catch (const std::exception &e){
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+  
   SYCLACADEMY_ASSERT(true);
 }
