6dad67d278233501ee1f4a8129371fce795d0acf
Prithvi <prithviraj645@gmail.com>
2026-01-07 03:16:35 -0500

    Finished ex06: Data Parallelism

--- Code_Exercises/Data_Parallelism/source.cpp
+++ Code_Exercises/Data_Parallelism/source.cpp
@@ -39,7 +39,12 @@
  *
 */
 
+#include <exception>
 #include "../helpers.hpp"
+
+#include <sycl/nd_item.hpp>
+#include <sycl/nd_range.hpp>
+#include <sycl/sycl.hpp>
 
 int main() {
   constexpr size_t dataSize = 1024;
@@ -52,9 +57,47 @@ int main() {
   }
 
   // Task: Compute r[i] = a[i] + b[i] in parallel on the SYCL device
-  for (int i = 0; i < dataSize; ++i) {
-    r[i] = a[i] + b[i];
+  // for (int i = 0; i < dataSize; ++i) {
+  //   r[i] = a[i] + b[i];
+  // }
+  try{
+    auto q = sycl::queue([=](sycl::exception_list el){
+      for (auto e: el){
+        std::rethrow_exception(e);
+      }
+    });
+
+    auto buf_a = sycl::buffer(a, sycl::range<1>(dataSize));
+    auto buf_b = sycl::buffer(b, sycl::range<1>(dataSize));
+    auto buf_r = sycl::buffer(r, sycl::range<1>(dataSize));
+
+    q.submit([&](sycl::handler &cgh){
+      auto acc_a = sycl::accessor(buf_a, cgh);
+      auto acc_b = sycl::accessor(buf_b, cgh);
+      auto acc_r = sycl::accessor(buf_r, cgh);
+
+      // simple sycl::range based kernel
+      cgh.parallel_for(sycl::range<1>(dataSize), [=](sycl::id<1> i){
+        acc_r[i] = acc_a[i] + acc_b[i];
+      });
+
+      // simple size_t based kernel
+      // cgh.parallel_for(dataSize, [=](sycl::id<1> i){
+      //   acc_r[i] = acc_a[i] + acc_b[i];
+      // });
+
+      // sycl::nd_range based kernel (potentially deprecated)
+      // cgh.parallel_for(sycl::nd_range<1>(dataSize, 1), [=](sycl::nd_item<1> item){
+      //   auto i = item.get_global_linear_id();
+      //   acc_r[i]  = acc_a[i] + acc_b[i];
+      // }); 
+    }).wait();
+  } catch (std::exception &e){
+    std::cout << "Error: " << e.what() << std::endl;
   }
+
+  // no need to copy back the data since buffer automatically does so to host
+  // when destroyed (provided it was initialized using host memory)
 
   SYCLACADEMY_ASSERT_EQUAL(r, [](size_t i) { return i * 2; });
 }
