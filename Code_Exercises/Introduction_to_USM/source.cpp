94ad39de0664b8d79359030c5f2d13492d40583a
Prithvi <prithviraj645@gmail.com>
2026-01-08 12:09:59 -0500

    Finished ex07: Introduction to USM

--- Code_Exercises/Introduction_to_USM/source.cpp
+++ Code_Exercises/Introduction_to_USM/source.cpp
@@ -8,6 +8,12 @@
  work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 */
 
+#include <exception>
+#include <ostream>
+#include <sycl/aspects.hpp>
+#include <sycl/exception.hpp>
+#include <sycl/exception_list.hpp>
+#include <sycl/info/info_desc.hpp>
 #include <sycl/sycl.hpp>
 
 #include "../helpers.hpp"
@@ -15,7 +21,22 @@
 int main() {
   // Task: create a queue to a device which supports USM allocations
   // Remember to check for exceptions
-  auto usmQueue = sycl::queue{};
+  try{
+    auto usmQueue = sycl::queue{[=](sycl::exception_list el){
+      for (auto e: el){
+        std::rethrow_exception(e);
+      }
+    }};
+
+    auto dev = usmQueue.get_device();
+    std::cout << "Device Name: " << (dev.get_info<sycl::info::device::name>()) << std::endl;
+    std::cout << "USM Device Allocations: " << (dev.has(sycl::aspect::usm_device_allocations) ? "True" : "False") << std::endl;
+    std::cout << "USM Host Allocations: " << (dev.has(sycl::aspect::usm_host_allocations) ? "True" : "False") << std::endl;
+    std::cout << "USM Shared Allocations: " << (dev.has(sycl::aspect::usm_shared_allocations) ? "True" : "False") << std::endl;
+    std::cout << "USM System Allocations: " << (dev.has(sycl::aspect::usm_system_allocations) ? "True" : "False") << std::endl;
+  } catch (std::exception &e){
+    std::cout << "Error: " << e.what() << std::endl;
+  }
 
   SYCLACADEMY_ASSERT(true);
 }
